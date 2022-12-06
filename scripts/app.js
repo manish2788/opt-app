56a1cc34a43e24407b7db462b4157589296e0dc9
Manish Kumar <manish.bb.kumar@oracle.com>
2022-12-06 17:22:11 +0530

    Updating use case

--- scripts/app.js
+++ scripts/app.js
@@ -1,119 +1,52 @@
-define(['persist/persistenceManager', 'persist/persistenceStoreManager', 'persist/defaultResponseProxy', 'persist/pouchDBPersistenceStoreFactory', 'persist/persistenceUtils', 'persist/fetchStrategies'], function (persistenceManager, persistenceStoreManager, defaultResponseProxy, pouchDBPersistenceStoreFactory, persistenceUtils, fetchStrategies) {
+define(['persist/persistenceManager', 'persist/persistenceStoreManager', 'persist/defaultResponseProxy', 'persist/pouchDBPersistenceStoreFactory', 'persist/persistenceUtils', 'persist/fetchStrategies', 'persist/oracleRestJsonShredding'], function (persistenceManager, persistenceStoreManager, defaultResponseProxy, pouchDBPersistenceStoreFactory, persistenceUtils, fetchStrategies, OracleRestJsonShredding) {
 
-  //let productUrl = "https://reqres.in/api/products/";
-  let productUrl = "http://localhost:3003/item";
-  let productEditUrl = "http://localhost:3003/dummy";
   let userUrl = "https://jsonplaceholder.typicode.com/todos";
-  let locationUrl = "http://localhost:3003/location";
-
-  let serverCallback = function(request, response) {
-    return Promise.resolve(response);
-  };
-//{serverResponseCallback: serverCallback}
   persistenceStoreManager.registerDefaultStoreFactory(pouchDBPersistenceStoreFactory);
-  //persistenceStoreManager.registerStoreFactory('location', pouchDBPersistenceStoreFactory);
   persistenceManager.init().then(function () {
     persistenceManager.register({
-      scope: '/location'
+      scope: '/todos'
     })
     .then(function (registration) {
       let responseProxy = defaultResponseProxy.getResponseProxy({
-        fetchStrategy: fetchStrategies.getCacheFirstStrategy()
+        jsonProcessor:
+          {
+              shredder: OracleRestJsonShredding.getShredder('todos'),
+              unshredder:  OracleRestJsonShredding.getUnshredder()
+          }
       });
       let fetchListener = responseProxy.getFetchEventListener();
-      //registration.addEventListener('fetch', fetchListener);
-      registration.addEventListener('fetch', function (event) {
-        event.respondWith(new Promise(function (resolve, reject) {
-          responseProxy.processRequest(event.request).then(function (response) {
-            if (response != null) {
-              // Check to see if the response from the cache is stale
-              console.log(response.headers.get('x-oracle-jscpt-cache-expiration-date'));
-              if ((new Date(response.headers.get('x-oracle-jscpt-cache-expiration-date'))).getTime() < (new Date()).getTime()) {
-                // handle response
-                console.log("handle response");
-              } else {
-                // handle stale response
-                console.log("handle stale response");
-              }
-            }
-            resolve(response);
-          });
-        }));
-      });
-    });
-
-    persistenceManager.register({
-      scope: '/item'
-    })
-    .then(function (registration) {
-      let responseProxy = defaultResponseProxy.getResponseProxy();
-      let fetchListener = responseProxy.getFetchEventListener();
-      registration.addEventListener('fetch', fetchListener);
-    });
-
-    persistenceManager.register({
-      scope: '/dummy'
-    })
-    .then(function (registration) {
-      let responseProxy = defaultResponseProxy.getResponseProxy();
-      let fetchListener = responseProxy.getFetchEventListener();
-      registration.addEventListener('fetch', fetchListener);
+      registration.addEventListener('fetch', fetchListener);;
     });
   });
 
   let userButton = document.getElementById('user');
+  let responseTable = document.getElementById('response');
+
+  let viewDBButton = document.getElementById('viewDB');
+  let dbDetail = document.getElementById('db-detail');
+
   userButton.addEventListener('click', ev => {
-    fetch(locationUrl)
-    .then(response => {
-      console.log(persistenceUtils.isCachedResponse(response));
-      return response.json()
-    })
-    .then(data => console.log(data));
-  });
-
-  let productsButton = document.getElementById('products');
-  productsButton.addEventListener('click', ev => {
-    fetch(productUrl)
-    .then(response => response.json())
-    .then(data => console.log(data));
-  });
-
-  let getLogButton = document.getElementById('getLog');
-  getLogButton.addEventListener('click', ev => {
-    persistenceManager.getSyncManager().getSyncLog().then(value => {
-      console.log(value);
-    })
-  });
-
-  let syncButton = document.getElementById('sync');
-  syncButton.addEventListener('click', ev => {
-    persistenceManager.getSyncManager().sync().then(value => {
-      console.log(value);
-    })
-  });
-
-  const postData = {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      "ID": 1,
-      "NAME": "First Name",
-      "EMAIL": "email@email.com",
-      "PHONE_NUMBER": "123-456-789",
-      "revNum": 0
-    })
-  };
-
-  let editButton = document.getElementById('edit');
-  editButton.addEventListener('click', ev => {
-    fetch(locationUrl, postData)
+    fetch(userUrl)
     .then(response => {
       return response.json()
     })
-    .then(data => console.log(data));
+    .then(data => {
+      console.log(data);
+      Object.keys(data).forEach(item => {
+        let template = `<tr><td>${data[item].id}</td><td>${data[item].title}</td></tr>`;
+        responseTable.insertAdjacentHTML("beforeEnd", template);
+      })
+    });
   });
 
+  viewDB.addEventListener('click', ev => {
+    const promise = window.indexedDB.databases()
+    promise.then((databases) => {
+      console.log(databases);
+      Object.keys(databases).forEach(item => {
+        let template = `<li>${databases[item].name}</li>`;
+        dbDetail.insertAdjacentHTML("beforeEnd", template);
+      });
+    })
+  });
 });
